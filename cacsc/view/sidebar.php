3ea4f9aa6feb2c40cd49c224a1a36c86b0086393
Cadrian Chan <cadrianchan@gmail.com>
2012-03-13 16:51:50 -0400

    improved front page appearance

---
+++ cacsc/view/sidebar.php
@@ -0,0 +1,6 @@
+<a href='index.php?rt=index'>Welcome</a> |
+<a href='index.php?rt=index/teams'>Teams</a> |
+<a href='index.php?rt=index/goals'>Goals</a> |
+<a href='index.php?rt=index/assists'>Assists</a> |
+<a href='index.php?rt=index/schedule'>Schedule/Results</a> &nbsp;&nbsp;&nbsp;
+<a href='index.php?rt=index/signup'>Sign up</a>
