d1324cf2333d0b837b8fd32709e5c6f27a05edb4
Cadrian Chan <cadrianchan@gmail.com>
2012-03-17 11:20:24 -0400

    updated index in friends table

--- setup.php
+++ setup.php
@@ -4,7 +4,7 @@ include_once "model/model.class.php";
 $model = new model($registry);
 $model->queryMysql("CREATE TABLE members(user VARCHAR(32), pass VARCHAR(16), INDEX(user(6)))");
 $model->queryMysql("CREATE TABLE messages(id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, auth VARCHAR(32), recip VARCHAR(32), pm CHAR(1), time INT UNSIGNED, message VARCHAR(4096), INDEX(auth(6)), INDEX(recip(6)))");
-$model->queryMysql("CREATE TABLE friends(user VARCHAR(32), follower VARCHAR(16), INDEX(user(6)), INDEX(friend(6)))");
+$model->queryMysql("CREATE TABLE friends(user VARCHAR(32), follower VARCHAR(16), INDEX(user(6)), INDEX(follower(6)))");
 $model->queryMysql("CREATE TABLE profiles(user VARCHAR(32), text VARCHAR(4096), INDEX(user(6)))");
 $model->queryMysql("CREATE TABLE stats(user VARCHAR(32), goals INT, assists INT, team VARCHAR(32), INDEX(user(6)))");
 $model->queryMysql("CREATE TABLE teams(teamnames VARCHAR(32))");
